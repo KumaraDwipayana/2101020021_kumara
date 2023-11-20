c0f6e76659c126350246dae2dcbf2f6f629ec031
KumaraDwipayana <97418740+KumaraDwipayana@users.noreply.github.com>
2023-11-20 10:39:05 +0800

    Add files via upload

---
+++ P10/sql.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "2101020021_kumara";
+
+    $con = mysqli_connect($server,$user,$password,$database);
+?>
